6e2fb50f9fa9a3d41769b9c23d417d6079077bd8
keewhy <samhoulahan625@gmail.com>
2017-04-18 14:04:42 +1200

    added animation

--- script.js
+++ script.js
@@ -1,45 +1,53 @@
 function draw() {
-  let topPosition = 0;
-  let leftposition = 0;
+
   let canvas = document.getElementById('canvas');
   let ctx = canvas.getContext('2d');
 
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
 
+  animate();
 
 
 
 
-
-let myRect = [];
-
-
-for (var i = 0; i < 20; i++) {
-  if (i <100){
-  myRect.push(new shape());
-  console.log(myRect);
-}
-}
+  let myRect = [];
 
 
+  for (var i = 0; i < 40; i++) {
+    if (i < 40) {
+      myRect.push(new shape());
+      console.log(myRect)
 
-function shape(){
 
-  let red = Math.random() * 255 >> 0;
-  let green = Math.random() * 255 >> 0;
-  let blue = Math.random() * 255 >> 0;
-  let color = "rgba(" + red + ", " + green + ", " + blue + ", 0.5)";
-  ctx.beginPath();
-  ctx.rect(50, 50, Math.floor((Math.random() * 950) + 250), Math.floor((Math.random() * 250) + 250));
-  ctx.fillStyle = color;
+    }
+  }
 
-  ctx.fill();
-  ctx.stroke();
+  function animate() {
+    setInterval(shape, 100); //Sets the loadtime between when a shape is rendered.
+  }
 
-  ctx.closePath();
 
-}
+
+  function shape() {
+
+    let red = Math.random() * 255 >> 0;  //Set colors to random
+    let green = Math.random() * 255 >> 0;
+    let blue = Math.random() * 255 >> 0;
+    let color = "rgba(" + red + ", " + green + ", " + blue + ", 0.3)";
+
+
+    ctx.beginPath();
+    ctx.rect(0, 0, Math.floor((Math.random() * 950) + 250), Math.floor((Math.random() * 650) + 250)); //randomise the size of the rectangle
+    ctx.fillStyle = color;
+    ctx.lineWidth=6;
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+  }
+
+
 
 
 
